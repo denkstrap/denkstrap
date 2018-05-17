05dff769b50f406ebcf5a502a519d901223189f1
Tatjana Roth <tatjana.roth@denkwerk.com>
2018-05-17 09:45:45 +0200

    add eslint package, update configs

---
+++ scripts/grunt/eslint.js
@@ -0,0 +1,31 @@
+/**
+ * Grunt ESLint plugin
+ * https://github.com/sindresorhus/grunt-eslint
+ */
+module.exports = {
+    frontend: [
+        '<%= srcPath %>**/*.js'
+    ],
+    grunt: [
+        '<%= gruntPath %>**/*.js',
+        'Gruntfile.js'
+    ],
+    karma: [
+        'karma.conf.js'
+    ],
+    ci: {
+        options: {
+            format: 'checkstyle',
+            outputFile: '<%= testResultPath %>eslint-lint-report.checkstyle.xml',
+            quiet: true
+        },
+        files: {
+            src: [
+                '<%= srcPath %>**/*.js',
+                '<%= gruntPath %>**/*.js',
+                '!<%= srcPath %>components/app/vendor/**/*.js',
+                'Gruntfile.js'
+            ]
+        }
+    }
+};
