4789334584d8a7caf4ee6ad5435a4ba58d62cb60
Tatjana Roth <tatjana.roth@denkwerk.com>
2018-06-15 22:47:35 +0200

    add eslint-loader to webpack/development.config

--- scripts/webpack/development.config.js
+++ scripts/webpack/development.config.js
@@ -1,3 +1,15 @@
 module.exports = {
-    devtool: 'eval-source-map'
+    devtool: 'eval-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.(js)$/,
+                enforce: 'pre',
+                exclude: /(node_modules|__*\.js)/,
+                use: [
+                    'eslint-loader',
+                ]
+            }
+        ]
+    }
 };
